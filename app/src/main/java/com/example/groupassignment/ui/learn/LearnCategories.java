9db3373ace4efef0f1953fc231f0735b4096a0b3
phillipsys <pilipus456@gmail.com>
2020-04-19 17:21:44 +1000

    Learn UI

--- app/src/main/java/com/example/groupassignment/ui/learn/LearnCategories.java
+++ app/src/main/java/com/example/groupassignment/ui/learn/LearnCategories.java
@@ -1,17 +1,21 @@
 package com.example.groupassignment.ui.learn;
 
+import com.example.groupassignment.R;
+
 import java.util.ArrayList;
 
 public class LearnCategories {
     private int level;
     private String categoryName;
+    private int image;
 
     public LearnCategories() {
     }
 
-    public LearnCategories(int level, String categoryName) {
+    public LearnCategories(int level, String categoryName, int image) {
         this.level = level;
         this.categoryName = categoryName;
+        this.image = image;
     }
 
     public String getCategoryName() {
@@ -31,18 +35,25 @@ public class LearnCategories {
         this.level = level;
     }
 
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
 
     public static ArrayList<LearnCategories> getCategories() {
         ArrayList<LearnCategories> categories = new ArrayList<>();
-        categories.add(new LearnCategories(1, "General"));
-        categories.add(new LearnCategories(2, "Conversation"));
-        categories.add(new LearnCategories(3, "Understanding"));
-        categories.add(new LearnCategories(4, "Purchase"));
-        categories.add(new LearnCategories(5, "Transport"));
-        categories.add(new LearnCategories(6, "Food & Drinks"));
-        categories.add(new LearnCategories(7, "Directions"));
-        categories.add(new LearnCategories(8, "Accommodation"));
-        categories.add(new LearnCategories(9, "Emergency"));
+        categories.add(new LearnCategories(1, "General", R.drawable.general));
+        categories.add(new LearnCategories(2, "Conversation", R.drawable.conversation));
+        categories.add(new LearnCategories(3, "Understanding", R.drawable.understand));
+        categories.add(new LearnCategories(4, "Purchase", R.drawable.purchase));
+        categories.add(new LearnCategories(5, "Transport", R.drawable.transport));
+        categories.add(new LearnCategories(6, "Food & Drinks", R.drawable.burger));
+        categories.add(new LearnCategories(7, "Directions", R.drawable.directions));
+        categories.add(new LearnCategories(8, "Accommodation", R.drawable.accomodation));
+        categories.add(new LearnCategories(9, "Emergency", R.drawable.emergency));
         return categories;
     }
 }
